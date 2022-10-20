52253a96e9c7ef36ee55ab37302370f95d8a5197
Stephan93uh <stephan9@hawaii.edu>
2022-10-19 21:49:34 -1000

    Completed Part 1-8

--- Hackwich7/ViewController.swift
+++ Hackwich7/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.text = ""
     }
 
-
+    @IBAction func setLabelButtonPressed(_ sender: Any) {
+        let userInputText = textField.text
+        
+        self.displayLabel.text = userInputText
+    }
+    
 }
 
